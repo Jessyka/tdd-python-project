3abff23f8d9b4cc66f72081149d65a44ae5808d0
Jessyka Lage <jessykalage@gmail.com>
2020-03-31 19:49:37 -0300

    improve tests

--- src/leilao/test_dominio.py
+++ src/leilao/test_dominio.py
@@ -4,21 +4,54 @@ from src.leilao.dominio import Usuario, Lance, Leilao, Avaliador
 
 
 class TestAvaliador(TestCase):
-    def test_avalia(self):
-        primeiro_usuario = Usuario("Gui")
-        segundo_usuario = Usuario("Pedro")
+    def setUp(self):
+        self.primeiro_usuario = Usuario("Gui")
+        self.segundo_usuario = Usuario("Pedro")
+        self.terceiro_usuario = Usuario("Vini")
 
-        lance_do_gui = Lance(primeiro_usuario, 150.00)
-        lance_do_pedro = Lance(segundo_usuario, 100.00)
+        self.lance_do_pedro = Lance(self.segundo_usuario, 100.00)
+        self.lance_do_gui = Lance(self.primeiro_usuario, 150.00)
+        self.lance_do_vini = Lance(self.terceiro_usuario, 80.00)
 
-        leilao = Leilao("Celular")
+        self.leilao = Leilao("Celular")
 
-        leilao.lances.append(lance_do_pedro)
-        leilao.lances.append(lance_do_gui)
+
+    def test_deve_retornar_maior_e_menor_valor_quando_adicionado_em_ordem_crescente(self):
+        self.leilao.lances.append(self.lance_do_pedro)
+        self.leilao.lances.append(self.lance_do_gui)
 
         avaliador = Avaliador()
-
-        avaliador.avalia(leilao)
+        avaliador.avalia(self.leilao)
 
         self.assertEqual(100.00, avaliador.menor_lance)
+        self.assertEqual(150.00, avaliador.maior_lance)
+
+    def test_deve_retornar_maior_e_menor_valor_quando_adicionado_em_ordem_decrescente(self):
+        self.leilao.lances.append(self.lance_do_pedro)
+        self.leilao.lances.append(self.lance_do_gui)
+
+        avaliador = Avaliador()
+        avaliador.avalia(self.leilao)
+
+        self.assertEqual(100.00, avaliador.menor_lance)
+        self.assertEqual(150.00, avaliador.maior_lance)
+
+    def test_deve_retornar_o_mesmo_valor_para_maior_e_menor_lance_quando_adicionado_apenas_um_lance(self):
+        self.leilao.lances.append(self.lance_do_gui)
+
+        avaliador = Avaliador()
+        avaliador.avalia(self.leilao)
+
+        self.assertEqual(150.00, avaliador.menor_lance)
+        self.assertEqual(150.00, avaliador.maior_lance)
+
+    def test_deve_retornar_maior_e_menor_valor_quando_tiver_mais_de_dois_lances(self):
+        self.leilao.lances.append(self.lance_do_pedro)
+        self.leilao.lances.append(self.lance_do_gui)
+        self.leilao.lances.append(self.lance_do_vini)
+
+        avaliador = Avaliador()
+        avaliador.avalia(self.leilao)
+
+        self.assertEqual(80.00, avaliador.menor_lance)
         self.assertEqual(150.00, avaliador.maior_lance)
